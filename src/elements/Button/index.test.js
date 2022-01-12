2dad97c502b0f24e99b818e31ec657e86db4fcdf
Dliya' Zarror Nibros <adorableprogrammer@gmail.com>
2022-01-12 19:21:53 +0700

    dfasdf

--- src/elements/Button/index.test.js
+++ src/elements/Button/index.test.js
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import Button from "./index";
 import { BrowserRouter as Router } from "react-router-dom";
 
-test("Should not allowed click button is isDisabled is present", () => {
+test("Should not allowed click button is isDisabled is resent", () => {
   const { container } = render(<Button isDisabled></Button>);
 
   expect(container.querySelector("span.disabled")).toBeInTheDocument();
