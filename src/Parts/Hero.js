7ca23dc1a6eaa526585e7b069796b2a95beb3e88
Dliya' Zarror Nibros <adorableprogrammer@gmail.com>
2022-01-15 13:55:59 +0700

    categories

--- src/Parts/Hero.js
+++ src/Parts/Hero.js
@@ -83,19 +83,19 @@ export default function Hero(props) {
           </div>
         </div>
 
-        <div className="col-6 pl5">
+        <div className="col-6 pl-5">
           <div style={{ width: 520, height: 410 }}>
             <img
               src={ImageHero}
               alt="Room with couches"
               className="img-fluid position-absolute"
-              style={{ margin: "-30 0 0 -30", zIndex: 1 }}
+              style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
             />
             <img
               src={ImageHero_}
               alt="Room with couches frame"
               className="img-fluid position-absolute"
-              style={{ margin: "0 -15 -15 0" }}
+              style={{ margin: "0 -15px -15px 0" }}
             />
           </div>
         </div>
