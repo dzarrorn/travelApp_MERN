b468520431573df405d40f681b84f572595986e8
Dliya' Zarror Nibros <adorableprogrammer@gmail.com>
2022-01-19 18:10:56 +0700

    inputDate

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "assets/scss/style.scss";
 import LandingPage from "Pages/LandingPage";
+import Example from "Pages/Example";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Route path="/" component={LandingPage}></Route>
+        {/* <Route path="/" component={Example}></Route> */}
       </Router>
     </div>
   );
